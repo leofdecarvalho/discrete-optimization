3893277547416484fcd5e103180eabb733b78938
Leonardo <leofdecarvalho@gmail.com>
2016-11-21 00:02:30 -0200

    Discrete-Optimization project Structure with java and maven

---
+++ src/test/java/Solver.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The class <code>knapsack.SolverColor</code> is an implementation of a greedy algorithm to solve the knapsack problem.
+ *
+ */
+class SolverTest {
+
+    @Before
+    public void setUp(){
+
+    }
+}
